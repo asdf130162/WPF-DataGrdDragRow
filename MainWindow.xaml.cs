6492ca84432b9a2aa2a7161fa9d742c38f777c21
高天昊 <1301626696@qq.com>
2021-07-17 14:46:35 +0800

    create first master

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainModel vm;
+        public MainWindow()
+        {
+            vm = new MainModel();
+            this.DataContext = vm;
+            vm.ListItem = new ObservableCollection<TestModel>()
+            {
+                new TestModel(){Column1="123"},
+                new TestModel(){Column1="456"},
+                new TestModel(){Column1="789"},
+            };
+        }
+        public class MainModel : ViewModelBase
+        {
+            ObservableCollection<TestModel> _ListItem;
+
+            public ObservableCollection<TestModel> ListItem { get => _ListItem; set { UpdateProper(ref _ListItem, value); } }
+        }
+        public class TestModel : ViewModelBase
+        {
+            string column1;
+            string column2;
+            string column3;
+
+            public string Column1 { get => column1; set { UpdateProper(ref column1, value); } }
+            public string Column2 { get => column2; set { UpdateProper(ref column2, value); } }
+            public string Column3 { get => column3; set { UpdateProper(ref column3, value); } }
+        }
+    }
+}
---
+++ ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ViewModelBase
+    {
+        [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+        public class CallerMemberNameAttribute : Attribute
+        {
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void UpdateProper<T>(ref T properValue, T newValue, [CallerMemberName] string properName = "")
+        {
+            if (object.Equals(properValue, newValue))
+                return;
+
+            properValue = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properName));
+        }
+    }
+}
